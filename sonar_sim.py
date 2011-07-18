4a85cec82fccd42e3766278c958792e4435813b4
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-18 16:04:37 +0100

    Few improvements, starting intersection stuff.

--- sonar_sim.py
+++ sonar_sim.py
@@ -13,7 +13,8 @@ class sonar:
 
     def __init__(self, rng, step, loc, map_):
         self.ranges = [] # Distances to objects in the map on previous pulse
-        self.range = rng # Maximum range of the sonar pulse in cm
+        self.max_range = rng # Maximum range of the sonar pulse in cm
+        self.min_range = 4
         self.step = step # Angle moved by the sonar head between each pulse
         self.angle_range = 270 # Total angle that the sonar sweeps through
         self.initial_angle = 225 # Where the first pulse is directed from
@@ -24,11 +25,26 @@ class sonar:
     def get_ranges(self):
         angle_swept = 0
         while angle_swept < self.angle_range:
-            return point_distange(self.loc, point_at_angle)
+            return point_distance(self.loc, intersect_point)
             angle_swept += self.step
+            self.current_angle += self.step
+
+    def get_intersect_point(self):
+        for line in self.map.lines:
+            print line
 
     def point_distance(self, p1, p2):
-        return sqrt(pow(p1[0]-p2[0], 2) + pow((p1[1]-p2[1]), 2))
+        # multiply distance by 10 since each 1 represents 10cm
+        dist = 10 * sqrt(pow(p1[0]-p2[0], 2) + pow((p1[1]-p2[1]), 2))
+        if self.min_range < dist < self.max_range:
+            # pretend that we can't resolve distances farther than the
+            # sonar's range, and closer than a certain distance.
+            return -1
+        else:
+            return dist
+
+    def get_scan_line(self):
+        return Line(self.loc, point_at_angle(self.current_angle))
 
     def point_at_angle(self, degrees):
         # (x', y') = (x + r cos a, y + r sin a)
@@ -59,6 +75,6 @@ if __name__ == '__main__':
     simple_map.add_line(Line(Point(2,2), Point(40,2)))
     simple_map.add_line(Line(Point(40,2), Point(40,40)))
     sonar = sonar(50, 5, Point(20,20), simple_map)
-    print sonar.point_distance(Point(5,10), Point(20,40))
-    #sonar.get_ranges()
+    #print sonar.point_distance(Point(5,10), Point(20,40))
+    sonar.get_ranges()
     
