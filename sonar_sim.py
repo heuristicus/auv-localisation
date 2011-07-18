862779c3f8d69ca1741895ec152e75478f52305c
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-18 15:28:03 +0100

    Code for calculating point on a circle's circumference.

--- sonar_sim.py
+++ sonar_sim.py
@@ -18,12 +18,18 @@ class sonar:
         self.initial_angle = 225 # Where the first pulse is directed from
         self.map = map_ # Map to use the sonar in
         self.loc = loc # The starting position of the sonar in the map
+        self.current_angle = self.initial_angle
 
     def get_ranges(self):
         angle_swept = 0
         while angle_swept < angle_range:
             
             angle_swept += self.step
+
+    def point_at_angle(self, degrees):
+        # (x', y') = (x + r cos a, y + r sin a)
+        # x,y = centre point, r = radius, a = angle
+        return Point(self.loc[0] + (self.rng * math.cos(degrees), self.loc[1] + (self.rng * math.cos(degrees))
 
 class map_:
     # For the purposes of the simulation, each increment of 1 in the
@@ -42,12 +48,14 @@ class map_:
         
     def add_line(self, line):
         self.lines.append(line)
-        
+
 if __name__ == '__main__':
     print 'not implemented yet.'
     simple_map = map_()
     simple_map.add_line(Line(Point(2,2), Point(2,40)))
     simple_map.add_line(Line(Point(2,2), Point(40,2)))
     simple_map.add_line(Line(Point(40,2), Point(40,40)))
-    print simple_map
+    sonar = sonar()
+                                    
+           
     
