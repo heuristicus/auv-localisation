2c3ae708fc02f09d3b3d5457b23e22e980261167
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-18 15:13:53 +0100

    Started using part of sympy for geometry, added map representation printout.

--- sonar_sim.py
+++ sonar_sim.py
@@ -1,4 +1,5 @@
 #!/bin/python
+from sympy.geometry import Line, Point
 
 class particle:
 
@@ -11,7 +12,7 @@ class sonar:
 
     def __init__(self, rng, step, loc, map_):
         self.distances = [] # Distances to objects in the map on previous pulse
-        self.range = rng # Range of the sonar pulse
+        self.range = rng # Range of the sonar pulse in cm
         self.step = step # Angle moved by the sonar head between each pulse
         self.angle_range = 270 # Total angle that the sonar sweeps through
         self.initial_angle = 225 # Where the first pulse is directed from
@@ -20,18 +21,33 @@ class sonar:
 
     def get_ranges(self):
         angle_swept = 0
-        while angle_swept != angle_range:
+        while angle_swept < angle_range:
             
-
             angle_swept += self.step
 
-class map:
-    
+class map_:
+    # For the purposes of the simulation, each increment of 1 in the
+    # coordinate space represents 10cm. i.e. (20,5) is 2m from the
+    # origin in the x direction and 50cm from the origin in the y
+    # direction.
+
     def __init__(self):
         self.lines = []
-
+    
+    def __repr__(self):
+        rep = ""
+        for line in self.lines:
+            rep += str(line) + "\n"
+        return rep
+        
     def add_line(self, line):
         self.lines.append(line)
         
 if __name__ == '__main__':
     print 'not implemented yet.'
+    simple_map = map_()
+    simple_map.add_line(Line(Point(2,2), Point(2,40)))
+    simple_map.add_line(Line(Point(2,2), Point(40,2)))
+    simple_map.add_line(Line(Point(40,2), Point(40,40)))
+    print simple_map
+    
