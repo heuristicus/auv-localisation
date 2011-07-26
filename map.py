f4d8eceeb4a97dfbaa882c89a532bc1c8a21fdf4
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-26 12:42:07 +0100

    map added

---
+++ map.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+class map_:
+    # For the purposes of the simulation, each increment of 1 in the
+    # coordinate space represents 10cm. i.e. (20,5) is 2m from the
+    # origin in the x direction and 50cm from the origin in the y
+    # direction.
+
+    def __init__(self):
+        self.lines = []
+    
+    def __repr__(self):
+        rep = ""
+        for line in self.lines:
+            rep += str(line) + "\n"
+        return rep
+        
+    def add_line(self, line):
+        self.lines.append(line)
+
+    def draw(self, canvas):
+        for line in self.lines:
+            draw_line(canvas, line)
