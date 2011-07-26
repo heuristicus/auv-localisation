57f97dc7615b8702ee662cdc9c1653dcb54e0a74
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-26 13:14:35 +0100

    simple event callback on mouse click

--- map_creator.py
+++ map_creator.py
@@ -3,6 +3,14 @@ import os
 import sys
 from Tkinter import *
 
+
+def init():
+    global point_list
+    global click_count # messy, but whatever
+    point_list = []
+    click_count = 0
+    create_canvas()
+   
 def create_map():
     print 'b'
 
@@ -11,8 +19,15 @@ def create_canvas():
     global canvas
     canvas = Canvas(ms, width=1000, height=1000)
     canvas.pack()
+    canvas.bind("<Button-1>", m1down)
+    ms.after_idle(create_map)
     canvas.mainloop()
 
+def m1down(event):
+    global click_count
+    click_count = 0 if click_count == 1 else 1
+    point_list.extend([event.x, event.y])
+    print point_list, click_count
+
 if __name__ == '__main__':
-    create_canvas()
-    create_map()
+    init()
