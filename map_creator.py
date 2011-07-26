a1f55dea8856d72d2408a1898cd163b33aba8ff0
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-26 14:06:04 +0100

    can save drawn maps to file

--- map_creator.py
+++ map_creator.py
@@ -2,26 +2,55 @@
 import os
 import sys
 from Tkinter import *
+import tkFileDialog
 
 
 def init():
     global point_list
+    global sonar_pos
+    global pos_flag
     point_list = []
+    sonar_pos = (0,0)
+    pos_flag = 0
     create_canvas()
    
-def create_map():
-    print 'b'
-
 def create_canvas():
     ms = Tk()
     global canvas
-    canvas = Canvas(ms, width=1000, height=1000)
+
+    sv = Button(ms, text='Save', command=save_to_file)
+    sv.pack()
+    sn = Button(ms, text='Sonar pos', command=set_sonar_pos)
+    sn.pack()
+    canvas = Canvas(ms, width=800, height=800)
     canvas.pack()
     canvas.bind("<Button-1>", m1down)
-    ms.after_idle(create_map)
     canvas.mainloop()
 
+def set_sonar_pos():
+    global pos_flag
+    pos_flag = 1
+
+def save_to_file():
+    f = tkFileDialog.asksaveasfile(defaultextension='.map')
+    for val in point_list:
+        print val
+        f.write(str(val) + ' ')
+    f.close()
+
 def m1down(event):
+    # Might be nice to extend this to make it so that if you click
+    # close to another point (say within 5 units), then the point
+    # added is the same as the one that is close.
+    global pos_flag
+    if pos_flag:
+        
+        pos_flag = 0
+        global sonar_pos 
+        sonar_pos = (event.x, event.y)
+        print 'Setting sonar position to ' + str(sonar_pos)
+        return
+    
     point_list.extend([event.x, event.y])
     if len(point_list) % 4 is 0:
         canvas.create_line(*point_list[-4:])
