cb921e1d401516b4a74bb42ffeabf8c128f4f23e
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-05 11:04:32 +0100

    comments for math

--- s_math.py
+++ s_math.py
@@ -7,11 +7,10 @@ import sys, random
 class SonarMath:
 
     def get_intersect_point(self, location, scan_line, map_):
+        """Get the closest intersection point betweena scan line and a line which makes up the map to a location """
         x = []
         for line in map_.lines:
             x.append(scan_line.intersection(line))
-            #print x
-            
         min_dist = sys.maxint
         closest = None
         for c in x:
@@ -23,42 +22,47 @@ class SonarMath:
     
     def intersect_distance(self, location, intersect, minrange, maxrange):
         if not intersect:
-            #print 'no intersection point to check distance to'
             return -1
         else:
             dist = intersect.distance(location)
-            #print 'distance is %d'%(dist)
             if minrange < dist < maxrange:
                 return dist
             else:
-                #print 'Distance not within tolerated range.'
+                # Distance not in tolerated range so return an 'error' reading.
                 return -1
 
     def get_scan_line(self, location, angle, length):
+        """Get a line between a central point and a point on a circle of radius length centred on the location, where the line is rotated by the given angle"""
         pt = self.point_at_angle(location, angle, length)
         ln = LineString([location.coords[0], pt.coords[0]])
         return ln
        
     def point_at_angle(self, centre, degrees, radius):
+        """Get the point at a given angle on a circle"""
         # (x', y') = (x + r cos a, y + r sin a)
         # x,y = centre point, r = radius, a = angle
         return Point(centre.x + (radius * cos(radians(degrees))), centre.y + (radius * sin(radians(degrees))))
 
     def apply_range_noise(self, lst, sigma):
+        """Apply gaussian noise to all values in a list of ranges"""
         noise_ranges = []
         for i in lst:
+            # only apply noise if the value is not an error value
             noise_ranges.append(random.gauss(i,sigma) if i is not -1 else -1)
         return noise_ranges
 
     def apply_point_noise(self, x, y, xsigma, ysigma):
+        """Apply gaussian noise to a point"""
         x = random.gauss(x, xsigma)
         y = random.gauss(y, ysigma)
         return (x,y)
 
     def get_noise(self, value, sigma):
+        """Get some gaussian noise"""
         return random.gauss(value, sigma)
 
     def get_move_vector(self, p1, p2):
+        """Get a vector that represents the movement from p1 to p2"""
         return (p2.x - p1.x, p2.y - p1.y)
 
     def rotate_point(self, centre, vector, angle):
@@ -72,11 +76,13 @@ class SonarMath:
         return Point(x,y)
 
     def gaussian(self, mu, sigma, x):
+        """Get the probability of the value x on a gaussian"""
         p1 = 1/(sqrt(2*pi*pow(sigma,2)))
         p2 = pow(e, ((-1*pow((x-mu),2)))/2.0*pow(sigma,2))
         return p1*p2
 
     def make_line(self, p1, p2):
+        """Make a line between the two points."""
         return LineString([(p1.x, p1.y),(p2.x, p2.y)])
 
 if __name__ == '__main__':
